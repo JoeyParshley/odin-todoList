2cc390a21dca80a775feb9b35f3fc9f0c70b29c4
Joey Parshley <joeyparshley@gmail.com>
2025-02-28 22:50:05 -0500

    Add npm scripts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import "./styles/style.css";
 import { greeting } from "./greeting.js";
-import joeyImage from "./images/joey.jpg";
-
-const image = document.createElement("img");
-image.src = joeyImage;
-
-document.body.appendChild(image);
 
 console.log(greeting);
