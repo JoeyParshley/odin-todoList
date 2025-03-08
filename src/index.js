11606b64455cffc4e8d617b037c2a4d0a1d33c81
Joey Parshley <joeyparshley@gmail.com>
2025-03-08 15:39:18 -0500

    Refactor project and tag list ui generation with dedicated functions

--- src/index.js
+++ src/index.js
@@ -9,15 +9,61 @@ import { Storage } from "./storage.js";
 
 const allProjects = JSON.parse(Storage.getProjects());
 
-// populate project list in the ui
-allProjects.forEach((project) => {
-    const projectList = document.querySelector(".project-list");
-    // create a new li element
-    const projectListLi = document.createElement("li");
-    // create a new a element
-    const projectListLink = document.createElement("a");
-    projectListLink.href = "#";
-    projectListLink.textContent = project.projectName;
-    projectListLi.appendChild(projectListLink);
-    projectList.appendChild(projectListLi);
-});
+/**
+ * Builds the project list in the ui
+ * @returns {void}
+ * @description This function loops through the projects and creates a new li element for each project. It then appends the li element to the project list.
+ * @example
+ * buildProjectList();
+ */
+function buildProjectList() {
+    allProjects.forEach((project) => {
+        const projectList = document.querySelector(".project-list");
+        // create a new li element
+        const projectListLi = document.createElement("li");
+        // create a new a element
+        const projectListLink = document.createElement("a");
+        projectListLink.href = "#";
+        projectListLink.textContent = project.projectName;
+        projectListLi.appendChild(projectListLink);
+        projectList.appendChild(projectListLi);
+    });
+}
+
+/**
+ * Gets all the tags from the projects
+ * @returns {Array} allTags
+ * @description This function loops through the projects and creates a new array of all the tags in the projects. It then returns the array.
+ * @example
+ * const allTags = getAllTagsFromProjects();
+ */
+function getAllTagsFromProjects() {
+    const allTags = [];
+    allProjects.forEach((project) => {
+        project.todos.forEach((todo) => {
+            todo.tags.forEach((tag) => {
+                if (!allTags.includes(tag)) {
+                    allTags.push(tag);
+                }
+            });
+        });
+    });
+    return allTags;
+}
+
+function buildTagList() {
+    const allTags = getAllTagsFromProjects();
+    const tagList = document.querySelector(".tag-list");
+    allTags.forEach((tag) => {
+        const tagListLi = document.createElement("li");
+        const tagListLink = document.createElement("a");
+        tagListLink.href = "#";
+        tagListLink.textContent = tag;
+        tagListLi.appendChild(tagListLink);
+        tagList.appendChild(tagListLi);
+    });
+}
+
+// build the project list
+buildProjectList();
+buildTagList();
