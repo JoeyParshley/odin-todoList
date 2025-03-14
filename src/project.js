89c74061036d700953b6f2fd81612918415a1661
Joey Parshley <joeyparshley@gmail.com>
2025-03-14 00:42:10 -0400

    Refactor project handling by consolidating project list and tag list building functions; update styles for active project indication

--- src/project.js
+++ src/project.js
@@ -1,35 +1,159 @@
+import { allProjects } from ".";
+import { Storage } from "./storage";
+import { buildTodoList } from "./todo";
+
 export class Project {
-  constructor(name, todos = []) {
-    this.name = name;
-    this.todos = todos;
-  }
+    constructor(name, todos = []) {
+        this.name = name;
+        this.todos = todos;
+    }
 
-  getName() {
-    return this.name;
-  }
+    getName() {
+        return this.name;
+    }
 
-  getTodos() {
-    return this.todos;
-  }
+    getTodos() {
+        return this.todos;
+    }
 
-  setName(name) {
-    this.name = name;
-  }
+    setName(name) {
+        this.name = name;
+    }
 
-  setTodos(newTodos) {
-    this.todos = [...newTodos];
-  }
+    setTodos(newTodos) {
+        this.todos = [...newTodos];
+    }
 
-  addTodo(todo) {
-    this.todos.push(todo);
-  }
+    addTodo(todo) {
+        this.todos.push(todo);
+    }
 
-  deleteTodoByName(name) {
-    this.todos = this.todos.filter((todo) => todo.title !== name);
-  }
+    deleteTodoByName(name) {
+        this.todos = this.todos.filter((todo) => todo.title !== name);
+    }
 
-  moveTodoToProject(todoToMove, newProject) {
-    this.todos = this.todos.filter((todo) => todo.title !== todoToMove.title);
-    newProject.todos.push(todoToMove);
-  }
+    moveTodoToProject(todoToMove, newProject) {
+        this.todos = this.todos.filter(
+            (todo) => todo.title !== todoToMove.title
+        );
+        newProject.todos.push(todoToMove);
+    }
+}
+
+/**
+ * @param {*} e
+ * @description This function handles the click event on the project list. It gets the project id from the data-project-id attribute and then gets the project from the storage. It then calls the showProjectDetails function to display the project details in the ui.
+ * @example
+ * handleProjectClick(e);
+ * @returns {void}
+ * TODO: refactor this function to use the project id to get the project from the storage and then call the showProjectDetails function to display the project details in the ui.
+ */
+export function buildProjectDetailsDom(e) {
+    e.preventDefault();
+    const target = e.target;
+    if (target.tagName === "A") {
+        const projectId = target.getAttribute("data-project-id");
+        // remove the active class from all the projects
+        document.querySelectorAll(".project-list li a").forEach((a) => {
+            a.classList.remove("active");
+        });
+        // add the active class to the clicked project
+        target.classList.add("active");
+        const project = Storage.getProjectById(projectId);
+        showProjectDetails(project);
+    }
+}
+
+/**
+ * Builds the project list in the ui
+ * @returns {void}
+ * @description This function loops through the projects and creates a new li element for each project. It then appends the li element to the project list.
+ * @example
+ * buildProjectList();
+ * @returns {void}
+ *
+ */
+export function buildProjectList() {
+    allProjects.forEach((project) => {
+        const projectList = document.querySelector(".project-list");
+        // create a new li element
+        const projectListItem = document.createElement("li");
+        // create a new a element
+        const projectListItemLink = document.createElement("a");
+        projectListItemLink.setAttribute("data-project-id", project.id);
+        projectListItemLink.href = "#";
+        projectList.addEventListener("click", buildProjectDetailsDom);
+        projectListItemLink.textContent = project.projectName;
+        projectListItem.appendChild(projectListItemLink);
+        projectList.appendChild(projectListItem);
+    });
+}
+
+/**
+ * Gets all the tags from the projects
+ * @returns {Array} allTags
+ * @description This function loops through the projects and creates a new array of all the tags in the projects. It then returns the array.
+ * @example
+ * const allTags = getAllTagsFromProjects();
+ */
+export function getAllTagsFromProjects() {
+    const allTags = [];
+    allProjects.forEach((project) => {
+        project.todos.forEach((todo) => {
+            todo.tags.forEach((tag) => {
+                if (!allTags.includes(tag)) {
+                    allTags.push(tag);
+                }
+            });
+        });
+    });
+    return allTags;
+}
+/**
+ * Builds the tag list in the ui
+ * @returns {void}
+ * @description This function loops through the tags and creates a new li element for each tag. It then appends the li element to the tag list.
+ * @example
+ * buildTagList();
+ */
+export function buildTagList() {
+    const allTags = getAllTagsFromProjects();
+    const tagList = document.querySelector(".tag-list");
+    allTags.forEach((tag) => {
+        const tagListLi = document.createElement("li");
+        const tagListLink = document.createElement("a");
+        tagListLink.href = "#";
+        tagListLink.textContent = tag;
+        tagListLi.appendChild(tagListLink);
+        tagList.appendChild(tagListLi);
+    });
+}
+/**
+ *
+ * @param {*} project
+ * @description This function takes a project object and displays the project details in the ui. It also clears the todos wrapper and appends the todos to the todos wrapper.
+ * @example
+ * showProjectDetails(project);
+ * @returns {void}
+ * TODO: refactor this function to use the project id to get the project from the storage and then call the showProjectDetails function to display the project details in the ui.
+ */
+
+export function showProjectDetails(project) {
+    const projectTitleH3 = document.querySelector("#project-title");
+    const projectTitle = project.projectName;
+    const todosWrapper = document.querySelector("#todos-wrapper");
+    const todosH3 = document.createElement("h3");
+    const todosList = document.createElement("ul");
+    const projectTodos = project.todos;
+
+    projectTitleH3.textContent = projectTitle;
+    todosList.classList.add("todo-list");
+    todosH3.textContent = "Todos";
+    // clear the todos wrapper
+    todosWrapper.innerHTML = "";
+    // append the h3 to the todos wrapper
+    todosWrapper.appendChild(todosH3);
+    todosWrapper.appendChild(todosList);
+
+    buildTodoList(project);
 }
--- src/todo.js
+++ src/todo.js
@@ -1,88 +1,185 @@
+import { Storage } from "./storage";
 import { tagManager } from "./tagManager.js";
 
 export class Todo {
-  constructor(title, description, dueDate, priority, notes, tags = []) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.notes = notes;
-    this.isComplete = false;
-    this.tags = [];
-    this.checklist = [];
-    tags.forEach((tag) => this.addTag(tag));
-  }
-
-  getTitle() {
-    return this.title;
-  }
-
-  getDescription() {
-    return this.description;
-  }
-
-  getDueDate() {
-    return this.dueDate;
-  }
-
-  getPriority() {
-    return this.priority;
-  }
-
-  getNotes() {
-    return this.notes;
-  }
-
-  getIsComplete() {
-    return this.isComplete;
-  }
-
-  setTitle(title) {
-    this.title = title;
-  }
-
-  setDescription(description) {
-    this.description = description;
-  }
-
-  setDueDate(dueDate) {
-    this.dueDate = dueDate;
-  }
-
-  setPriority(priority) {
-    this.priority = priority;
-  }
-
-  setNotes(notes) {
-    this.notes = notes;
-  }
-
-  setIsComplete(isComplete) {
-    this.isComplete = isComplete;
-  }
-
-  toggleComplete() {
-    this.isComplete = !this.isComplete;
-  }
-
-  addTag(tag) {
-    if (!this.tags.includes(tag)) {
-      this.tags.push(tag);
-      tagManager.addTag(tag);
+    constructor(title, description, dueDate, priority, notes, tags = []) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.isComplete = false;
+        this.tags = [];
+        this.checklist = [];
+        tags.forEach((tag) => this.addTag(tag));
     }
-  }
 
-  addCheckListItem(item) {
-    this.checklist.push({ item, completed: false });
-  }
-
-  toggleChecklistItem(index) {
-    if (this.checklist[index]) {
-      this.checklist[index].completed = !this.checklist[index].completed;
+    getTitle() {
+        return this.title;
     }
-  }
 
-  save() {
-    localStorage.setItem(this.title, JSON.stringify(this));
-  }
+    getDescription() {
+        return this.description;
+    }
+
+    getDueDate() {
+        return this.dueDate;
+    }
+
+    getPriority() {
+        return this.priority;
+    }
+
+    getNotes() {
+        return this.notes;
+    }
+
+    getIsComplete() {
+        return this.isComplete;
+    }
+
+    setTitle(title) {
+        this.title = title;
+    }
+
+    setDescription(description) {
+        this.description = description;
+    }
+
+    setDueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    setPriority(priority) {
+        this.priority = priority;
+    }
+
+    setNotes(notes) {
+        this.notes = notes;
+    }
+
+    setIsComplete(isComplete) {
+        this.isComplete = isComplete;
+    }
+
+    toggleComplete() {
+        this.isComplete = !this.isComplete;
+    }
+
+    addTag(tag) {
+        if (!this.tags.includes(tag)) {
+            this.tags.push(tag);
+            tagManager.addTag(tag);
+        }
+    }
+
+    addCheckListItem(item) {
+        this.checklist.push({ item, completed: false });
+    }
+
+    toggleChecklistItem(index) {
+        if (this.checklist[index]) {
+            this.checklist[index].completed = !this.checklist[index].completed;
+        }
+    }
+
+    save() {
+        localStorage.setItem(this.title, JSON.stringify(this));
+    }
+}
+
+/**
+ * Displays the details of a todo item in the UI.
+ *
+ * @param {Object}          todo             The todo item to display.
+ * @param {string}          todo.title       The title of the todo item.
+ * @param {string}          todo.description The description of the todo item.
+ * @param {string}          todo.dueDate     The due date of the todo item.
+ * @param {string}          todo.priority    The priority of the todo item.
+ * @param {string}          todo.notes       Additional notes for the todo item.
+ * @param {Array<string>}   todo.tags        Tags associated with the todo item.
+ * @param {string}          projectName      The name of the project the todo item belongs to.
+ */
+export function showTodoDetails(todo, projectName) {
+    const breadcrumbsElement = document.querySelector("#breadcrumbs");
+    const todoNameH3 = document.querySelector("#detail-name");
+    const todoDescriptionP = document.querySelector("#detail-description");
+    const todoDueDateP = document.querySelector("#detail-dueDate");
+    const todoPriorityP = document.querySelector("#detail-priority");
+    const todoNotesP = document.querySelector("#detail-notes");
+    const todoTagsWrapper = document.querySelector("#detail-tags");
+
+    breadcrumbsElement.textContent = `My Projects > ${projectName}`;
+    todoNameH3.textContent = "";
+    todoNameH3.textContent = todo.title;
+    todoDescriptionP.textContent = "";
+    todoDescriptionP.textContent = todo.description;
+    todoDueDateP.textContent = "";
+    todoDueDateP.textContent = todo.dueDate;
+    todoPriorityP.textContent = "";
+    todoPriorityP.textContent = todo.priority;
+    todoNotesP.textContent = "";
+    todoNotesP.textContent = todo.notes;
+    todoTagsWrapper.innerHTML = "";
+    todo.tags.forEach((tag) => {
+        const span = document.createElement("span");
+        span.classList.add("tag");
+        span.textContent = `#${tag}`;
+        todoTagsWrapper.appendChild(span);
+    });
+}
+
+/**
+ * Handles the click event on a todo item link, updates the active state,
+ * retrieves the corresponding todo and project details, and displays them.
+ *
+ * @param {Event} e - The event object from the click event.
+ */
+export function buildTodoDetailsDom(e) {
+    e.preventDefault();
+    const target = e.target;
+    if (target.tagName === "A") {
+        const todoId = target.getAttribute("data-todo-id");
+        const projectId = target.getAttribute("data-project-id");
+        document.querySelectorAll(".todo-list li a").forEach((a) => {
+            a.classList.remove("active");
+        });
+        target.classList.add("active");
+        const currentProject = Storage.getProjectById(projectId);
+        const currentTodo = currentProject.todos.find((todo) => {
+            return todo.id === parseInt(todoId, 10);
+        });
+        showTodoDetails(currentTodo, currentProject.projectName);
+    }
+}
+
+/**
+ * Builds a list of todo items for a given project and appends them to the DOM.
+ *
+ * @param {Object} project - The project object containing todos.
+ * @param {Array} project.todos - The array of todo items.
+ * @param {string} project.id - The unique identifier for the project.
+ *
+ * Each todo item in the project.todos array should have the following structure:
+ * @param {Object} project.todos[].id - The unique identifier for the todo item.
+ * @param {string} project.todos[].title - The title of the todo item.
+ */
+export function buildTodoList(project) {
+    project.todos.forEach((todo) => {
+        const todoList = document.querySelector("#todos-wrapper .todo-list");
+        const todoListItem = document.createElement("li");
+        const todoListItemLink = document.createElement("a");
+        const todoCheckbox = document.createElement("div");
+        todoCheckbox.classList.add("checkbox");
+        todoListItemLink.href = "#";
+        todoListItemLink.classList.add("todo-title");
+        todoListItemLink.setAttribute("data-todo-id", todo.id);
+        todoListItemLink.setAttribute("data-project-id", project.id);
+        todoListItemLink.textContent = todo.title;
+        todoList.addEventListener("click", buildTodoDetailsDom);
+        todoListItem.classList.add("todo-item");
+        todoListItem.appendChild(todoCheckbox);
+        todoListItem.appendChild(todoListItemLink);
+        todoList.appendChild(todoListItem);
+    });
 }
