83b647e110133c9f9c9d941a9cc43e053cae4ea5
Joey Parshley <joeyparshley@gmail.com>
2025-04-07 21:05:34 -0400

    Implement edit mode for todo details with save and cancel functionality
    update styles to show editable fields

--- src/showTodoDetails.js
+++ src/showTodoDetails.js
@@ -23,6 +23,11 @@ export function showTodoDetails(todo, project) {
     const todoNotesP = document.querySelector("#detail-notes");
     const todoTagsWrapper = document.querySelector("#detail-tags");
     const todoCheckbox = document.querySelector(".details.checkbox");
+    const editButton = document.querySelector("#edit-button");
+
+    editButton.addEventListener("click", (e) => {
+        toggleEditMode(e, todo);
+    });
 
     todoCheckbox.addEventListener("click", (e) =>
         toggleCompletedStatus(e, project.id, todo.id)
@@ -36,7 +41,7 @@ export function showTodoDetails(todo, project) {
     }
 
     /**
-     * TODO: when there is more than todo associated with the tag the `todo` is undefined in for the first todo in the todo-list
+     * TODO: when there is more than one todo associated with the tag the `todo` is undefined in for the first todo in the todo-list
      *
      * Need a way to handle, multiple projects/todos when checking the filtered links so can display all project/todo titles associated with the tag
      */
@@ -77,4 +82,91 @@ export const setFieldContent = (element, todo, todoKey) => {
         e.target.removeAttribute("contenteditable");
         console.log(`todo[${todoKey}] = ${todo[todoKey]}`);
     });
+};
+
+/**
+ * Toggles the edit mode for the todo details section in the UI.
+ *
+ * @param {Event} e - The event object triggered by the user interaction.
+ * @param {Object} todo - The todo object being edited.
+ * @param {string} todo.title - The title of the todo item.
+ * @param {string} todo.description - The description of the todo item.
+ * @param {string} todo.dueDate - The due date of the todo item.
+ * @param {string} todo.priority - The priority of the todo item.
+ * @param {string} todo.notes - Additional notes for the todo item.
+ * @param {Array<string>} todo.tags - Tags associated with the todo item.
+ *
+ * @returns {void}
+ *
+ * @description
+ * This function toggles the edit mode for the todo details section. When entering edit mode,
+ * it hides the edit button, makes the fields editable, and adds "Cancel" and "Save" buttons.
+ * When exiting edit mode, it restores the original state by removing the "Cancel" and "Save"
+ * buttons, disabling content editing, and showing the edit button again.
+ *
+ * The "Cancel" button exits edit mode without saving changes, while the "Save" button
+ * triggers a placeholder save logic and then exits edit mode.
+ */
+
+export const toggleEditMode = (e, todo) => {
+    const todoDetails = document.querySelector("#todo-details");
+    const editButton = document.querySelector("#edit-button");
+    let cancelButton = document.querySelector("#cancel-button");
+    let saveButton = document.querySelector("#save-button");
+    const isEditing = todoDetails.classList.contains("editing");
+
+    if (isEditing) {
+        // Exit edit mode
+
+        todoDetails.classList.remove("editing");
+        editButton.classList.remove("hidden");
+
+        // Remove cancel and save buttons
+        if (cancelButton) cancelButton.remove();
+        if (saveButton) saveButton.remove();
+
+        const editableFields = todoDetails.querySelectorAll(
+            "[contenteditable='true']"
+        );
+        editableFields.forEach((field) => {
+            field.removeAttribute("contenteditable");
+        });
+    } else {
+        // Enter edit mode
+        todoDetails.classList.add("editing");
+        // Hide edit button
+        editButton.classList.add("hidden");
+
+        // Create cancel button
+        cancelButton = document.createElement("button");
+        cancelButton.id = "cancel-button";
+        cancelButton.textContent = "Cancel";
+        cancelButton.addEventListener("click", () => toggleEditMode(e, todo));
+        todoDetails.appendChild(cancelButton);
+
+        // Create save button
+        saveButton = document.createElement("button");
+        saveButton.id = "save-button";
+        saveButton.textContent = "Save";
+        saveButton.addEventListener("click", () => {
+            // Save logic here
+            console.log("Save changes");
+            toggleEditMode(e, todo);
+        });
+        todoDetails.appendChild(saveButton);
+
+        const fieldsToEdit = [
+            "#detail-name",
+            "#detail-description",
+            "#detail-dueDate",
+            "#detail-priority",
+            "#detail-notes",
+        ];
+        fieldsToEdit.forEach((selector) => {
+            const field = document.querySelector(selector);
+            if (field) {
+                field.setAttribute("contenteditable", "true");
+            }
+        });
+    }
 };
